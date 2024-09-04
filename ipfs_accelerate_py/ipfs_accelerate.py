58cb69fd0bb3193f897cb2f8e4cec7caf5a664ea
endomorphosis <starworks5@gmail.com>
2024-09-03 22:09:32 -0700

    chore: Update ipfs_accelerate_py package to version 0.0.8

--- ipfs_accelerate_py/ipfs_accelerate.py
+++ ipfs_accelerate_py/ipfs_accelerate.py
@@ -8,26 +8,26 @@ class load_checkpoint_and_dispatch:
 
     def __call__(self):
 
-        if self.device_map == "auto":
-            import torch
-            self.model.load_state_dict(torch.load(self.checkpoint))
-        elif self.device_map == "lilypad":
-            from .backends import backends
-            docker_tunnel = backends.lilypad(self.model, self.checkpoint)
-            self.model.load_state_dict(torch.load(self.checkpoint, map_location="cpu"))
-        elif self.device_map == "akash":
-            from .backends import backends
-            docker_tunnel = backends.akash(self.model, self.checkpoint)
-            self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
-        elif self.device_map == "vast":
-            from .backends import backends
-            docker_tunnel = backends.vast(self.model, self.checkpoint)
-            self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
-        elif self.device_map == "hallucinate":
-            from .backends import backends
-            docker_tunnel = backends.hallucinate(self.model, self.checkpoint)
-            self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
-        else:
-            raise ValueError("Invalid device_map")
+        # if self.device_map == "auto":
+        #     import torch
+        #     self.model.load_state_dict(torch.load(self.checkpoint))
+        # elif self.device_map == "lilypad":
+        #     from .backends import backends
+        #     docker_tunnel = backends.lilypad(self.model, self.checkpoint)
+        #     self.model.load_state_dict(torch.load(self.checkpoint, map_location="cpu"))
+        # elif self.device_map == "akash":
+        #     from .backends import backends
+        #     docker_tunnel = backends.akash(self.model, self.checkpoint)
+        #     self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
+        # elif self.device_map == "vast":
+        #     from .backends import backends
+        #     docker_tunnel = backends.vast(self.model, self.checkpoint)
+        #     self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
+        # elif self.device_map == "hallucinate":
+        #     from .backends import backends
+        #     docker_tunnel = backends.hallucinate(self.model, self.checkpoint)
+        #     self.model.load_state_dict(torch.load(self.checkpoint, map_location="cuda:0"))
+        # else:
+        #     raise ValueError("Invalid device_map")
 
-        return self.model
+        return self
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='ipfs_accelerate_py',
-	version='0.0.7',
+	version='0.0.8',
 	packages=[
         'ipfs_accelerate_py',
 	],
