90f047e832d238f1de26ef7b9ec25ee588afcd54
Benjamin Barber <starworks5@gmail.com>
2024-11-05 18:37:10 -0800

    chore: Bump version to 0.0.16 in setup.py and pyproject.toml; add __init__.py for install_depends

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='ipfs_accelerate_py',
-	version='0.0.15',
+	version='0.0.16',
     packages=find_packages(),
 	install_requires=[
         'transformers',
