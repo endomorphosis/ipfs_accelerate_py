40adb9a46f5adde45fb73652d637a5073826b1b4
Benjamin Barber <starworks5@gmail.com>
2024-11-11 03:06:19 -0800

    refactor: update background task calls to use model_server methods

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI, BackgroundTasks
-from pydantic import BaseModel
 from ipfs_accelerate_py import ipfs_accelerate_py
+from pydantic import BaseModel
 
 class TestEndpointRequest(BaseModel):
     models: list[str]
@@ -73,24 +73,24 @@ model_server = ModelServer()
 
 @app.get("/add_endpoint")
 async def add_endpoint(request: AddEndpointRequest, background_tasks: BackgroundTasks):
-    BackgroundTasks.add_task(addEndpointTask, request.models, request.resources)
+    BackgroundTasks.add_task(model_server.addEndpointTask, request.models, request.resources)
     return {"message": "add endpoint started in the background"}
 
 @app.get("/rm_endpoint")
 async def rm_endpoint(request: RmEndpointRequest, background_tasks: BackgroundTasks):
-    BackgroundTasks.add_task(rmEndpointTask, request.models)
+    BackgroundTasks.add_task(model_server.rmEndpointTask, request.models)
     return {"message": "rm endpoint started in the background"}
 
 @app.get("/status")
 async def status_post(request: InitStatusRequest, background_tasks: BackgroundTasks):
-    BackgroundTasks.add_task(statusTask, request.models)
+    BackgroundTasks.add_task(model_server.statusTask, request.models)
     return {"message": "status started in the background"}
 
 @app.post("/init")
 async def load_index_post(request: InitEndpointsRequest, background_tasks: BackgroundTasks):
     results = {}
     try:
-        results["init"] = await model_server.initEndpointsTask(request.models , request.resources)
+        results["init"] = await model_server.initEndpointsTask(request.models, request.resources)
     except Exception as e:
         results["init"]  = e
     return results
@@ -99,12 +99,12 @@ async def load_index_post(request: InitEndpointsRequest, background_tasks: Backg
 
 @app.post("/test")
 async def search_item_post(request: TestEndpointRequest, background_tasks: BackgroundTasks):
-    BackgroundTasks.add_task(testEndpointTask, request.models, request.resources)
+    BackgroundTasks.add_task(model_server.testEndpointTask, request.models, request.resources)
     return {"message": "test started in the background"}
 
 @app.post("/infer")
 async def infer(request: InferEndpointRequest, background_tasks: BackgroundTasks):
-    BackgroundTasks.add_task(inferTask, request.models, request.batch_data)
+    BackgroundTasks.add_task(model_server.inferTask, request.models, request.batch_data)
     return {"message": "infer started in the background"}
 
 @app.post("/")
