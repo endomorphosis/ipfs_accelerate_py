873188cde5c07414109214d27ce49b19075b5f7c
Benjamin Barber <starworks5@gmail.com>
2024-11-13 19:31:17 -0800

    refactor: remove unused modules and clean up codebase

--- ipfs_accelerate_py/__init__.py
+++ ipfs_accelerate_py/__init__.py
@@ -1,6 +1,5 @@
 from .worker import worker
 from .config import config
-from .endpoints import endpoints_py
 from .install_depends import install_depends
 from .test_ipfs_accelerate import test_ipfs_accelerate
 from .ipfs_accelerate import ipfs_accelerate_py
@@ -8,7 +7,6 @@ export = {
     "config": config,
     "test_ipfs_accelerate": test_ipfs_accelerate,
     "install_depends": install_depends,
-    "endpoints": endpoints,
     "ipfs_accelerate_py": ipfs_accelerate_py,
     "worker": worker
 }
--- ipfs_accelerate_py/libp2p_kit/s3_kit.py
+++
@@ -1,3 +0,0 @@
-class s3_kit:
-    def __init__(self):
-        pass
