680aa59125b07ccdb00b8061e5757582b1920889
Benjamin Barber <starworks5@gmail.com>
2024-12-15 01:03:12 -0800

    refactor: Clean up commented-out code in worker.py for better readability

--- ipfs_accelerate_py/worker/worker.py
+++ ipfs_accelerate_py/worker/worker.py
@@ -257,8 +257,6 @@ class worker_py:
         # elif "hf_embed" in globals():
         #     self.hf_embed = hf_embed
         
-
-        
         # if "should_abort" not in globals() and "should_abort" not in list(self.resources.keys()):
         #     self.should_abort = should_abort(resources, metadata)
         # elif "should_abort" in list(self.resources.keys()):
@@ -337,8 +335,6 @@ class worker_py:
             config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
             model_type = config.__class__.model_type
         return model_type
-    
-    
     
     async def get_openvino_model(self, model_name, model_type=None, device_name=None ):
         architecture = None
@@ -558,71 +554,9 @@ class worker_py:
 
     #     return return_model_type
     
-    # def create_vlm_endpoint_handler(self, endpoint_model, cuda_label):
-    #     return self.hf_llava.create_vlm_endpoint_handler(endpoint_model, cuda_label)
-
-    # def create_vlm_endpoint_handler(self, endpoint_model, cuda_label):
-    #     def handler(x):
-    #         if "eval" in dir(self.local_endpoints[endpoint_model][cuda_label]):
-    #             self.local_endpoints[endpoint_model][cuda_label].eval()
-    #         else:
-    #             pass
-    #         with torch.no_grad():
-    #             try:
-    #                 torch.cuda.empty_cache()
-    #                 # Tokenize input with truncation and padding
-    #                 # tokens = self.tokenizer[endpoint_model][cuda_label](
-    #                 #     x, 
-    #                 #     return_tensors='pt', 
-    #                 #     padding=True, 
-    #                 #     truncation=True,
-    #                 #     max_length=self.local_endpoints[endpoint_model][cuda_label].config.max_position_embeddings
-    #                 # )
-    #                 config = AutoConfig.from_pretrained(endpoint_model, trust_remote_code=True)
-    #                 tokens = self.tokenizer[endpoint_model][cuda_label](x, return_tensors='pt', padding=True, truncation=True, max_length=512)
-                    
-    #                 # Move tokens to the correct device
-    #                 input_ids = tokens['input_ids'].to(self.local_endpoints[endpoint_model][cuda_label].device)
-    #                 attention_mask = tokens['attention_mask'].to(self.local_endpoints[endpoint_model][cuda_label].device)
-                    
-    #                 # Run model inference
-    #                 outputs = self.local_endpoints[endpoint_model][cuda_label](
-    #                     input_ids=input_ids,
-    #                     attention_mask=attention_mask,
-    #                     return_dict=True
-    #                 )
-                        
-    #                 # Process and prepare outputs
-    #                 if hasattr(outputs, 'last_hidden_state'):
-    #                     hidden_states = outputs.last_hidden_state.cpu().numpy()
-    #                     attention_mask_np = attention_mask.cpu().numpy()
-    #                     result = {
-    #                         'hidden_states': hidden_states,
-    #                         'attention_mask': attention_mask_np
-    #                     }
-    #                 else:
-    #                     result = outputs.to('cpu').detach().numpy()
-
-    #                 # Cleanup GPU memory
-    #                 del tokens, input_ids, attention_mask, outputs
-    #                 if 'hidden_states' in locals(): del hidden_states
-    #                 if 'attention_mask_np' in locals(): del attention_mask_np
-    #                 torch.cuda.empty_cache()
-    #                 return result
-    #             except Exception as e:
-    #                 # Cleanup GPU memory in case of error
-    #                 if 'tokens' in locals(): del tokens
-    #                 if 'input_ids' in locals(): del input_ids
-    #                 if 'attention_mask' in locals(): del attention_mask
-    #                 if 'outputs' in locals(): del outputs
-    #                 if 'hidden_states' in locals(): del hidden_states
-    #                 if 'attention_mask_np' in locals(): del attention_mask_np
-    #                 torch.cuda.empty_cache()
-    #                 raise e
-    #     return handler
     
-    def create_endpoint_handler(self, endpoint_model, cuda_label):
-        return self.default.create_endpoint_handler(endpoint_model, cuda_label)
+    # def create_endpoint_handler(self, endpoint_model, cuda_label):
+    #     return self.default.create_endpoint_handler(endpoint_model, cuda_label)
     
     # def create_endpoint_handler(self, endpoint_model, cuda_label):
     #     def handler(x):
@@ -686,41 +620,6 @@ class worker_py:
         def handler(x):
             return self.local_endpoints[endpoint_model][openvino_label](x)
         return handler
-
-    # def create_openvino_vlm_endpoint_handler(self, endpoint_model, openvino_label):
-    #     return self.hf_llava.create_openvino_vlm_endpoint_handler(endpoint_model, openvino_label)
-
-    # def create_openvino_vlm_endpoint_handler(self, endpoint_model, openvino_label):
-    #     def handler(x, y=None):
-    #         chat = None
-    #         image_file = None
-    #         if y is not None and x is not None:
-    #             chat, image_file = x
-    #         elif x is not None:
-    #             if type(x) == tuple:
-    #                 chat, image_file = x
-    #             elif type(x) == list:
-    #                 chat = x[0]
-    #                 image_file = x[1]
-    #             elif type(x) == dict:
-    #                 chat = x["chat"]
-    #                 image_file = x["image"]
-    #             elif type(x) == str:
-    #                 chat = x
-    #             else:
-    #                 pass
-                
-    #         image = load_image(image_file)
-    #         prompt = self.local_endpoints[endpoint_model][openvino_label].apply_chat_template(chat, add_generation_prompt=True)
-    #         inputs = self.local_endpoints[endpoint_model][openvino_label](images=image, text=prompt, return_tensors="pt")
-    #         streamer = TextStreamer(self.tokenizer[endpoint_model][openvino_label], skip_prompt=True, skip_special_tokens=True)
-    #         output_ids = self.local_endpoints[endpoint_model][openvino_label].generate(
-    #             **inputs,
-    #             do_sample=False,
-    #             max_new_tokens=50,
-    #             streamer=streamer,
-    #         )
-    #     return handler
     
     async def init_worker(self, models, local_endpoints, hwtest):
         if local_endpoints is None or len(local_endpoints) == 0:
