f6fb6de235fbc8a4599d59fc4a800d1376750d7e
endomorphosis <starworks5@gmail.com>
2025-01-08 01:26:19 -0800

    feat: Refactor endpoint handler methods for MLM integration in hf_t5 class

--- ipfs_accelerate_py/worker/skillset/hf_t5.py
+++ ipfs_accelerate_py/worker/skillset/hf_t5.py
@@ -37,7 +37,7 @@ class hf_t5:
         except Exception as e:
             print(e)
             pass
-        endpoint_handler = self.create_llm_endpoint_handler(endpoint, tokenizer, model, cuda_label)
+        endpoint_handler = self.create_mlm_endpoint_handler(endpoint, tokenizer, model, cuda_label)
         torch.cuda.empty_cache()
         # batch_size = await self.max_batch_size(endpoint_model, cuda_label)
         return endpoint, tokenizer, endpoint_handler, asyncio.Queue(64), 0
@@ -49,11 +49,65 @@ class hf_t5:
         batch_size = 0                
         tokenizer =  AutoTokenizer.from_pretrained(model, use_fast=True, trust_remote_code=True)
         endpoint = get_openvino_model(model, model_type, openvino_label)
-        endpoint_handler = self.create_openvino_llm_endpoint_handler(endpoint,tokenizer, model, openvino_label)
+        endpoint_handler = self.create_openvino_mlm_endpoint_handler(endpoint,tokenizer, model, openvino_label)
         batch_size = 0
         return endpoint, tokenizer, endpoint_handler, asyncio.Queue(64), batch_size          
     
-    def create_llm_endpoint_handler(self, local_cuda_endpoint, local_cuda_processor, endpoint_model, cuda_label):
+    def create_cuda_mlm_endpoint_handler(self, local_cuda_endpoint, local_cuda_processor, endpoint_model, cuda_label):
+        def handler(x, y=None, local_cuda_endpoint=local_cuda_endpoint, local_cuda_processor=local_cuda_processor, endpoint_model=endpoint_model, cuda_label=cuda_label):
+            # if "eval" in dir(self.local_endpoints[endpoint_model][cuda_label]):
+            #       self.local_endpoints[endpoint_model][cuda_label].eval()
+            if "eval" in dir(local_cuda_endpoint):
+                local_cuda_endpoint.eval()
+            else:
+                pass
+            with torch.no_grad():
+                try:
+                    torch.cuda.empty_cache()
+                    config = AutoConfig.from_pretrained(endpoint_model, trust_remote_code=True)
+                    
+                    if x is not None and type(x) == str:
+                        conversation = [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {"type": "image"},
+                                    {"type": "text", "text": x},
+                                ],
+                            },
+                        ]
+                    elif type(x) == tuple:
+                        conversation = x
+                    elif type(x) == dict:
+                        raise Exception("Invalid input to vlm endpoint handler")
+                    elif type(x) == list:
+                        # conversation = x
+                        conversation = [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {"type": "image"},
+                                    {"type": "text", "text": x},
+                                ],
+                            },
+                        ]
+                    else:
+                        raise Exception("Invalid input to vlm endpoint handler")
+                  
+                    prompt = local_cuda_processor.apply_chat_template(conversation, add_generation_prompt=True)
+                    inputs = local_cuda_processor(prompt, return_tensors="pt").to(cuda_label, torch.float16)
+                    output = local_cuda_endpoint.generate(**inputs, max_new_tokens=30)
+                    result = local_cuda_processor.batch_decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+                    # Run model inference
+                    torch.cuda.empty_cache()
+                    return result
+                except Exception as e:
+                    # Cleanup GPU memory in case of error
+                    torch.cuda.empty_cache()
+                    raise e
+        return handler
+    
+    def create_cpu_mlm_endpoint_handler(self, local_cuda_endpoint, local_cuda_processor, endpoint_model, cuda_label):
         def handler(x, y=None, local_cuda_endpoint=local_cuda_endpoint, local_cuda_processor=local_cuda_processor, endpoint_model=endpoint_model, cuda_label=cuda_label):
             # if "eval" in dir(self.local_endpoints[endpoint_model][cuda_label]):
             #       self.local_endpoints[endpoint_model][cuda_label].eval()
@@ -107,7 +161,7 @@ class hf_t5:
                     raise e
         return handler
 
-    def create_openvino_llm_endpoint_handler(self, openvino_endpoint_handler, openvino_tokenizer, endpoint_model, openvino_label):
+    def create_openvino_mlm_endpoint_handler(self, openvino_endpoint_handler, openvino_tokenizer, endpoint_model, openvino_label):
         def handler(x, y=None, openvino_endpoint_handler=openvino_endpoint_handler, openvino_tokenizer=openvino_tokenizer, endpoint_model=endpoint_model, openvino_label=openvino_label):
             chat = None
             if y is not None and x is not None:
