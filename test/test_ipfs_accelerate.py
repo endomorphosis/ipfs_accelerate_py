2fecbcd8f981bc0ef529e384f2e86fc553aa373e
endomorphosis <endomorphosis@users.noreply.github.com>
2025-01-31 21:12:58 -0800

    test: Add main execution block and resource metadata for backend testing

--- test/test_ipfs_accelerate.py
+++ test/test_ipfs_accelerate.py
@@ -3,24 +3,44 @@ import os
 import sys
 
 class test_ipfs_accelerate:
-    def __init__(self, resources, metadata):
-        self.resources = resources
-        self.metadata = metadata
+    def __init__(self, resources=None, metadata=None):
+        
         if self.resources is None:
             self.resources = {}
+        else:
+            self.resources = resources
+        
         if self.metadata is None:
             self.metadata = {}
-        if "ipfs_accelerate_py" not in globals() and "ipfs_accelerate_py" not in list(self.resources.keys()):
-            from ipfs_accelerate import ipfs_accelerate_py
-            self.resources["ipfs_accelerate_py"] = ipfs_accelerate_py(resources, metadata)
-        elif "test_backend" in list(self.resources.keys()):
-            self.test_backend = self.resources["test_backend"]
+        else:
+            self.metadata = metadata
+        
+        if "ipfs_accelerate_py" not in dir(self):
+            if "ipfs_accelerate_py" not in list(self.resources.keys()):
+                from ipfs_accelerate_py import ipfs_accelerate_py
+                self.resources["ipfs_accelerate_py"] = ipfs_accelerate_py(resources, metadata)
+                self.ipfs_accelerate_py = self.resources["ipfs_accelerate_py"]
+            else:
+                self.ipfs_accelerate_py = self.resources["ipfs_accelerate_py"]
+        
+        if "test_backend" not in dir(self):
+            if "test_backend" not in list(self.resources.keys()):
+                from test_backend import test_backend
+                self.resources["test_backend"] = test_backend(resources, metadata)
+                self.test_backend = self.resources["test_backend"]
+            else:
+                self.test_backend = self.resources["test_backend"]
+        
         return None
     
     async def test(self):
         test_results = {}
         try:
-            test_results["test_ipfs_accelerate"] = self.ipfs_accelerate.__test__(resources, metadata)
+            # test_results["test_ipfs_accelerate"] = self.ipfs_accelerate.__test__(resources, metadata)
+            results = {}
+            ipfs_accelerate_init = await self.ipfs_accelerate_py.init_endpoints( metadata['models'], resources)
+            test_endpoints = await self.ipfs_accelerate_py.test_endpoints(metadata['models'], ipfs_accelerate_init)
+            return test_endpoints
         except Exception as e:
             test_results["test_ipfs_accelerate"] = e
         
@@ -34,83 +54,167 @@ class test_ipfs_accelerate:
     async def test_ipfs_accelerate(self):
         test_results = {}
         try:
-            test_results["test_ipfs_accelerate"] = self.ipfs_acclerate.__test__(resources, metadata)
-            return test_results 
+            ipfs_accelerate_py = self.ipfs_accelerate_py(resources, metadata)
+            # test_results["test_ipfs_accelerate"] = await ipfs_acclerate.__test__(resources, metadata)
+            ipfs_accelerate_init = ipfs_accelerate_py.init_endpoints( metadata['models'], resources)
+            test_endpoints = ipfs_accelerate_py.test_endpoints(metadata['models'], ipfs_accelerate_init)
+            return test_endpoints 
         except Exception as e:
             test_results["test_ipfs_accelerate"] = e
             return test_results
     
-
 if __name__ == "__main__":
     metadata = {
-        "dataset": "TeraflopAI/Caselaw_Access_Project",
-        "namespace": "TeraflopAI/Caselaw_Access_Project",
-        "column": "text",
+        "dataset": "laion/gpt4v-dataset",
+        "namespace": "laion/gpt4v-dataset",
+        "column": "link",
+        "role": "master",
         "split": "train",
         "models": [
-            "thenlper/gte-small",
-            # "Alibaba-NLP/gte-large-en-v1.5",
-            # "Alibaba-NLP/gte-Qwen2-1.5B-instruct",
+            # "laion/larger_clap_general",
+            "google-t5/t5-base",
+            # "facebook/wav2vec2-large-960h-lv60-self",
+            # "BAAI/bge-small-en-v1.5", 
+            # "openai/clip-vit-base-patch16",  ## fix audio tensor and check that the right format is being used for whisper models in the test Can't set the input tensor with index: 0, because the model input (shape=[?,?]) and the tensor (shape=(0)) are incompatible  
+            # "openai/whisper-large-v3-turbo",
+            # "meta-llama/Meta-Llama-3.1-8B-Instruct",
+            # "distil-whisper/distil-large-v3",
+
+
+            # "Qwen/Qwen2-7B",
+            # "llava-hf/llava-interleave-qwen-0.5b-hf",
+            # "lmms-lab/LLaVA-Video-7B-Qwen2",
+            # "llava-hf/llava-v1.6-mistral-7b-hf",
+            # "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+            # "TIGER-Lab/Mantis-8B-siglip-llama3",  ## make sure sthat optimum-cli-convert works on windows.
+            # "microsoft/xclip-base-patch16-zero-shot",
+            # "google/vit-base-patch16-224"
+
+
+            # "MCG-NJU/videomae-base",
+            # "MCG-NJU/videomae-large",
+            # "laion/CLIP-ViT-H-14-laion2B-s32B-b79K",   ## openclip not yet supported
+            # "lmms-lab/llava-onevision-qwen2-7b-si",  
+            # "lmms-lab/llava-onevision-qwen2-7b-ov", 
+            # "lmms-lab/llava-onevision-qwen2-0.5b-si", 
+            # "lmms-lab/llava-onevision-qwen2-0.5b-ov", 
+            # "Qwen/Qwen2-VL-7B-Instruct", ## convert_model() ->   ('Couldn\'t get TorchScript module by scripting. With exception:\nComprehension ifs are not supported yet:\n  File "/home/devel/.local/lib/python3.12/site-packages/transformers/models/qwen2_vl/modeling_qwen2_vl.py", line 1187\n    \n        if not return_dict:\n            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)\n        return BaseModelOutputWithPast(\n            last_hidden_state=hidden_states,\n\n\nTracing sometimes provide better results, please provide valid \'example_input\' argument. You can also provide TorchScript module that you obtained yourself, please refer to PyTorch documentation: https://pytorch.org/tutorials/beginner/Intro_to_TorchScript_tutorial.html.',)
+            # "OpenGVLab/InternVL2_5-1B", ## convert_model() -> torchscript error Couldn't get TorchScript module by scripting. With exception: try blocks aren't supported:
+            # "OpenGVLab/InternVL2_5-8B", ## convert_model() -> torchscript error Couldn't get TorchScript module by scripting. With exception: try blocks aren't supported:
+            # "OpenGVLab/PVC-InternVL2-8B", ## convert_model() -> torchscript error Couldn't get TorchScript module by scripting. With exception: try blocks aren't supported:
+            # "AIDC-AI/Ovis1.6-Llama3.2-3B", # ValueError: Trying to export a ovis model, that is a custom or unsupported architecture,
+            # "BAAI/Aquila-VL-2B-llava-qwen", # Asked to export a qwen2 model for the task visual-question-answering (auto-detected), but the Optimum OpenVINO exporter only supports the tasks feature-extraction, feature-extraction-with-past, text-generation, text-generation-with-past, text-classification for qwen2. Please use a supported task. Please open an issue at https://github.com/huggingface/optimum/issues if you would like the task visual-question-answering to be supported in the ONNX export for qwen2.
         ],
         "chunk_settings": {
-            "chunk_size": 512,
-            "n_sentences": 8,
-            "step_size": 256,
-            "method": "fixed",
-            "embed_model": "thenlper/gte-small",
-            "tokenizer": None
+
         },
-        "dst_path": "/storage/teraflopai/tmp",
+        "path": "/storage/gpt4v-dataset/data",
+        "dst_path": "/storage/gpt4v-dataset/data",
     }
     resources = {
         "local_endpoints": [
-            ["thenlper/gte-small", "cpu", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "cpu", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "cpu", 32768],
-            ["thenlper/gte-small", "cuda:0", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "cuda:0", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "cuda:0", 32768],
-            ["thenlper/gte-small", "cuda:1", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "cuda:1", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "cuda:1", 32768],
-            ["thenlper/gte-small", "openvino", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "openvino", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "openvino", 32768],
-            ["thenlper/gte-small", "llama_cpp", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "llama_cpp", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "llama_cpp", 32768],
-            ["thenlper/gte-small", "ipex", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "ipex", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "ipex", 32768],
+            ["google-t5/t5-base", "cpu", 32768],
+            ["openai/whisper-large-v3-turbo", "cpu", 32768],
+            ["MCG-NJU/videomae-base", "cpu", 32768],
+            ["microsoft/xclip-base-patch16-zero-shot", "cpu", 32768],
+            ["MCG-NJU/videomae-large", "cpu", 32768],
+            ["BAAI/bge-small-en-v1.5", "cpu", 32768],
+            ["laion/larger_clap_general", "cpu", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "cpu", 32768],
+            ["openai/clip-vit-base-patch16", "cpu", 32768],
+            ["laion/CLIP-ViT-H-14-laion2B-s32B-b79K", "cpu", 32768],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "cpu", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "cpu", 32768],
+            ["TIGER-Lab/Mantis-8B-siglip-llama3", "cpu", 32768],
+            ["lmms-lab/llava-onevision-qwen2-7b-si", "cpu", 32768],
+            ["lmms-lab/llava-onevision-qwen2-7b-ov", "cpu", 32768],
+            ["lmms-lab/LLaVA-Video-7B-Qwen2", "cpu", 32768],
+            ["meta-llama/Meta-Llama-3.1-8B-Instruct", "cpu", 32768],
+            ["Qwen/Qwen2-7B", "cpu", 32768],
+            ["llava-hf/llava-interleave-qwen-0.5b-hf", "cpu", 32768],
+            ["lmms-lab/llava-onevision-qwen2-0.5b-si", "cpu", 32768],
+            ["lmms-lab/llava-onevision-qwen2-0.5b-ov", "cpu", 32768],
+            ["Qwen/Qwen2-VL-7B-Instruct", "cpu", 32768],
+            ["OpenGVLab/InternVL2_5-1B", "cpu", 32768],
+            ["OpenGVLab/InternVL2_5-8B", "cpu", 32768],
+            ["OpenGVLab/PVC-InternVL2-8B", "cpu", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "cpu", 32768],
+            ["laion/larger_clap_general", "cpu", 32768],
+            ["openai/clip-vit-base-patch16", "cpu", 32768],
+            ["BAAI/bge-small-en-v1.5", "cpu", 32768],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "cpu", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "cpu", 32768],
+            ["distil-whisper/distil-large-v3", "cpu", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "cuda:0", 32768],
+            ["google-t5/t5-base", "cuda:0", 32768],
+            ["openai/whisper-large-v3-turbo", "cuda:0", 32768],
+            ["MCG-NJU/videomae-base", "cuda:0", 32768],
+            ["microsoft/xclip-base-patch16-zero-shot", "cuda:0", 32768],
+            ["MCG-NJU/videomae-large", "cuda:0", 32768],
+            ["BAAI/bge-small-en-v1.5", "cuda:0", 32768],
+            ["laion/larger_clap_general", "cuda:0", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "cuda:0", 32768],
+            ["openai/clip-vit-base-patch16", "cuda:0", 32768],
+            ["laion/CLIP-ViT-H-14-laion2B-s32B-b79K", "cuda:0", 32768],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "cuda:0", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "cuda:0", 32768],
+            ["TIGER-Lab/Mantis-8B-siglip-llama3", "cuda:0", 32768],
+            ["TIGER-Lab/Mantis-8B-siglip-llama3", "openvino:0", 32768],
+            ["lmms-lab/llava-onevision-qwen2-7b-si", "cuda:0", 32768],
+            ["lmms-lab/llava-onevision-qwen2-7b-ov", "cuda:0", 32768],
+            ["lmms-lab/LLaVA-Video-7B-Qwen2", "cuda:0", 32768],
+            ["meta-llama/Meta-Llama-3.1-8B-Instruct", "cuda:0", 32768],
+            ["Qwen/Qwen2-7B", "cuda:0", 32768],
+            ["Qwen/Qwen2-7B", "openvino:0", 32768],
+            ["llava-hf/llava-interleave-qwen-0.5b-hf", "cuda:0", 32768],
+            ["lmms-lab/llava-onevision-qwen2-0.5b-si", "cuda:0", 32768],
+            ["lmms-lab/llava-onevision-qwen2-0.5b-ov", "cuda:0", 32768],
+            ["Qwen/Qwen2-VL-7B-Instruct", "cuda:0", 32768],
+            ["OpenGVLab/InternVL2_5-1B", "cuda:0", 32768],
+            ["OpenGVLab/InternVL2_5-8B", "cuda:0", 32768],
+            ["OpenGVLab/PVC-InternVL2-8B", "cuda:0", 32768],
+            ["laion/larger_clap_general", "cuda:0", 32768],
+            ["openai/clip-vit-base-patch16", "cuda:0", 32768],
+            ["BAAI/bge-small-en-v1.5", "cuda:0", 32768],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "cuda:0", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "cuda:0", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "cuda:1", 32768],
+            ["google-t5/t5-base", "cuda:1", 32768],
+            ["openai/whisper-large-v3-turbo", "cuda:1", 32768],
+            ["MCG-NJU/videomae-base", "cuda:1", 32768],
+            ["microsoft/xclip-base-patch16-zero-shot", "cuda:1", 32768],
+            ["MCG-NJU/videomae-large", "cuda:1", 32768],
+            ["BAAI/bge-small-en-v1.5", "cuda:1", 32768],
+            ["laion/larger_clap_general", "cuda:1", 32768],
+            ["openai/clip-vit-base-patch16", "cuda:1", 32768],
+            ["BAAI/bge-small-en-v1.5", "cuda:1", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "cuda:1", 32768],
+            ["distil-whisper/distil-large-v3", "cuda:1", 32768],
+            ["distil-whisper/distil-large-v3", "openvino:0", 32768],
+            ["google-t5/t5-base", "openvino:0", 32768],
+            ["openai/whisper-large-v3-turbo", "openvino:0", 32768],
+            ["MCG-NJU/videomae-base", "openvino:0", 32768],
+            ["microsoft/xclip-base-patch16-zero-shot", "openvino:0", 32768],
+            ["MCG-NJU/videomae-large", "openvino:0", 32768],
+            ["BAAI/bge-small-en-v1.5", "openvino:0", 32768],
+            ["laion/larger_clap_general", "openvino:0", 32768],
+            ["facebook/wav2vec2-large-960h-lv60-self", "openvino:0", 32768],
+            ["BAAI/bge-small-en-v1.5", "openvino:0", 32768],
+            ["openai/clip-vit-base-patch16", "openvino:0", 32768],
+            ["laion/larger_clap_general", "openvino:0", 32768],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "openvino:0", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "openvino:0", 32768],
+            ["BAAI/bge-small-en-v1.5", "llama_cpp", 512],
+            ["llava-hf/llava-v1.6-mistral-7b-hf", "llama_cpp", 8192],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "llama_cpp", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "ipex", 32768],
+            ["distil-whisper/distil-large-v3", "ipex", 32768],
+            ["TinyLlama/TinyLlama-1.1B-Chat-v1.0", "ipex", 32768],
         ],
-        "openvino_endpoints": [
-            # ["neoALI/bge-m3-rag-ov", "https://bge-m3-rag-ov-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-rag-ov/infer", 4095],
-            # ["neoALI/bge-m3-rag-ov", "https://bge-m3-rag-ov-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-rag-ov/infer", 4095],
-            # ["neoALI/bge-m3-rag-ov", "https://bge-m3-rag-ov-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-rag-ov/infer", 4095],
-            # ["neoALI/bge-m3-rag-ov", "https://bge-m3-rag-ov-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-rag-ov/infer", 4095],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx0-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx0/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx1-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx1/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx2-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx2/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx3-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx3/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx4-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx4/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx5-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx5/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx6-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx6/infer", 1024],
-            # ["aapot/bge-m3-onnx", "https://bge-m3-onnx7-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/bge-m3-onnx7/infer", 1024]
-        ],
-        "tei_endpoints": [
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://62.146.169.111:8080/embed-medium", 32768],
-            ["thenlper/gte-small", "http://62.146.169.111:8080/embed-tiny", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "http://62.146.169.111:8081/embed-small", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://62.146.169.111:8081/embed-medium", 32768],
-            ["thenlper/gte-small", "http://62.146.169.111:8081/embed-tiny", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "http://62.146.169.111:8082/embed-small", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://62.146.169.111:8082/embed-medium", 32768],
-            ["thenlper/gte-small", "http://62.146.169.111:8082/embed-tiny", 512],
-            ["Alibaba-NLP/gte-large-en-v1.5", "http://62.146.169.111:8083/embed-small", 8192],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://62.146.169.111:8083/embed-medium", 32768],
-            ["thenlper/gte-small", "http://62.146.169.111:8083/embed-tiny", 512]
-        ]
+        "openvino_endpoints": [],
+        "tei_endpoints": [],
     }
+
     ipfs_accelerate_py = ipfs_accelerate_py(resources, metadata)
     asyncio.run(ipfs_accelerate_py.__test__(resources, metadata))
     print("test complete")
