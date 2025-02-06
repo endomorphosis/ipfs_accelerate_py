361160fe3650b17fe248b854324de72bb128fc77
endomorphosis <endomorphosis@users.noreply.github.com>
2025-02-05 23:29:30 -0800

    feat: Expand test_api_backend with additional test methods for improved coverage

--- ipfs_accelerate_py/test/test_api_backend.py
+++ ipfs_accelerate_py/test/test_api_backend.py
@@ -2,28 +2,47 @@ class test_api_backend:
     def __init__(self, resources, metadata):
         self.resources = resources
         self.metadata = metadata
-        
+        self.test_hf_tgi = self.test_hf_tgi
+        self.test_openai_api = self.test_openai_api
+        self.test_groq = self.test_groq
+        self.test_s3_kit = self.test_s3_kit
+        self.test_hf_tei = self.test_hf_tei
+        self.test_llvm = self.test_llvm
+        self.test_ovms = self.test_ovms
+        self.test_ollama = self.test_ollama
+        return None
 
-    def test_webnn(self):
+    def test_ovms(self):
         return None
     
-    def test_cuda(self):
+    def test_ollama(self):
         return None
 
-    def test_openvino(self):
+    def test_hf_tgi(self):
         return None
     
-    def test_qualcomm(self):
+    def test_openai_api(self):
         return None
     
-    def test_apple(self):
+    def test_groq(self):
+        return None
+    
+    def test_s3_kit(self):
+        return None
+    
+    def test_hf_tei(self):
+        return None
+    
+    def test_llvm(self):
         return None
     
     def __test__(self):
-        self.test_webnn()   
-        self.test_cuda()
-        self.test_openvino()
-        self.test_qualcomm()
-        self.test_apple()
-        
+        self.test_ovms()
+        self.test_ollama()
+        self.test_hf_tgi()
+        self.test_openai_api()
+        self.test_groq()
+        self.test_s3_kit()
+        self.test_hf_tei()
+        self.test_llvm()
         return None
