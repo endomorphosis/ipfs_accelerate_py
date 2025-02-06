2c8b6a763decc06ddf237dacf8dd1e8264aff36c
endomorphosis <endomorphosis@users.noreply.github.com>
2025-02-05 23:38:58 -0800

    feat: Add placeholder comments for test dependencies in test_hardware_backend

--- ipfs_accelerate_py/test/test_hardware_backend.py
+++ ipfs_accelerate_py/test/test_hardware_backend.py
@@ -2,20 +2,26 @@ class test_hardware_backend:
     def __init__(self, resources, metadata):
         self.resources = resources
         self.metadata = metadata
+        return None
 
     def test_webnn(self):
+        ## test dependencies
         return None
     
     def test_cuda(self):
+        ## test dependencies
         return None
 
     def test_openvino(self):
+        ## test dependencies
         return None
     
     def test_qualcomm(self):
+        ## test dependencies
         return None
     
     def test_apple(self):
+        ## test dependencies
         return None
     
     def __test__(self):
