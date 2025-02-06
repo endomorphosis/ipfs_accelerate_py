d43c81359a9711551562f859b3092bd5ac269462
endomorphosis <endomorphosis@users.noreply.github.com>
2025-02-05 23:37:48 -0800

    feat: Add test cases for additional APIs in test_api_backend for improved coverage

--- ipfs_accelerate_py/test/test_api_backend.py
+++ ipfs_accelerate_py/test/test_api_backend.py
@@ -29,21 +29,27 @@ class test_api_backend:
         return None
 
     def test_hf_tgi(self):
+        self.apis.hf_tgi.__test__()
         return None
     
     def test_openai_api(self):
+        self.apis.openai_api.__test__()
         return None
     
     def test_groq(self):
+        self.apis.groq.__test__()
         return None
     
     def test_s3_kit(self):
+        self.apis.s3_kit.__test__()
         return None
     
     def test_hf_tei(self):
+        self.apis.hf_tei.__test__()
         return None
     
     def test_llvm(self):
+        self.apis.llvm.__test__()
         return None
     
     def __test__(self):
