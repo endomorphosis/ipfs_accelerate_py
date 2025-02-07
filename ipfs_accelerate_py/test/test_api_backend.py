36c7bba83f1ecf07f43966cfd62d78634e3f33d1
endomorphosis <endomorphosis@users.noreply.github.com>
2025-02-06 18:40:20 -0800

    feat: Refactor test_api_backend to return results and handle exceptions for each test

--- ipfs_accelerate_py/test/test_api_backend.py
+++ ipfs_accelerate_py/test/test_api_backend.py
@@ -53,12 +53,50 @@ class test_api_backend:
         return None
     
     def __test__(self):
-        self.test_ovms()
-        self.test_ollama()
-        self.test_hf_tgi()
-        self.test_openai_api()
-        self.test_groq()
-        self.test_s3_kit()
-        self.test_hf_tei()
-        self.test_llvm()
-        return None
+        results = {}
+        
+        try:
+            results["ovms"] =  self.test_ovms()
+        except Exception as e:
+            results["ovms"] = e
+        
+        try:
+            results["ollama"] =  self.test_ollama()
+        except Exception as e:
+            results["ollama"] = e
+        
+        try:
+            results["hf_tgi"] =  self.test_hf_tgi()
+        except Exception as e:
+            results["hf_tgi"] = e
+        
+        try:
+            results["openai_api"] =  self.test_openai_api()
+        except Exception as e:
+            results["openai_api"] = e
+        
+        try:
+            results["groq"] =  self.test_groq()
+        except Exception as e:
+            results["groq"] = e
+        
+        try:
+            results["s3_kit"] =  self.test_s3_kit()
+        except Exception as e:
+            results["s3_kit"] = e
+        
+        try:
+            results["hf_tei"] =  self.test_hf_tei()
+        except Exception as e:
+            results["hf_tei"] = e
+        
+        try:
+            results["llvm"] =  self.test_llvm()
+        except Exception as e:
+            results["llvm"] = e
+        
+        return results
+        
+if __name__ == "__main__":
+    test_api_backend(resources={}, metadata={}).__test__()
+    print("test_api_backend passed")
